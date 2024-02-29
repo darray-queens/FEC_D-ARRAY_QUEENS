a073ff5904637c595c436c988350346701023106
jreagant <jr.tankersley@gmail.com>
2024-02-29 13:26:57 -0500

    reverted to boilerplate

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,6 @@
 import React from 'react';
-import { useState } from 'react';
 
 const App = () => {
-
-  const [currentProduct, setCurrentProduct] = useState(null)
-
-  return <div>Howdy, world!!</div>;
+  return <div>Howdy, world!</div>;
 };
 export default App;
