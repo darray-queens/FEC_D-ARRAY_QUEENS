524d85ad18cc767a4f44ba047e30b8dd6f2879b0
Andrew <andrewpark0408@DESKTOP-H04QUCF.localdomain>
2024-02-29 15:33:27 -0800

    imported GetAllQuestions to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import { useState } from 'react';
+import GetAllQuestions from './QuestionsAndAnswers/GetAllQuestions.jsx'
 
 const App = () => {
 
   const [currentProduct, setCurrentProduct] = useState(null)
 
-  return <div>Howdy, world!!</div>;
+  return (
+  <div>
+    <h2>Howdy, world!!</h2>
+    <GetAllQuestions />
+    </div>
+
+  )
 };
 export default App;
