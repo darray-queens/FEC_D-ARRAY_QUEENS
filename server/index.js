623f825ee5b0d85ee290fdf86dd283f1477b55bd
jreagant <jr.tankersley@gmail.com>
2024-03-01 13:15:46 -0500

    fixed webpack config, app mount in index.jsx

--- server/index.js
+++ server/index.js
@@ -1,12 +1,15 @@
 require('dotenv').config();
+
 const path = require('path');
 const express = require('express');
+
+const port = process.env.PORT || 3000;
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, '../dist')));
 // other configuration...
 
-app.listen(3000, () => {
-  console.log('Listening on port 3000');
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
 });
