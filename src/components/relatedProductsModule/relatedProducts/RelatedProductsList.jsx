0dad9bdc37aba11fb1705b922de3dfc5998e14e4
Amarin Sam <amarinsam@gmail.com>
2024-03-01 17:37:40 -0800

    Rendered list of products tile and child Card components

--- src/components/relatedProductsModule/relatedProducts/RelatedProductsList.jsx
+++ src/components/relatedProductsModule/relatedProducts/RelatedProductsList.jsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import Card from '../shared/Card';
 
-function RelatedProductsList() {
+function RelatedProductsList({ currentProduct }) {
+  const [productsList, setProductsList] = useState([]);
+
+  console.log(currentProduct)
+
+  useEffect(() => {
+    axios.get('/products')
+      .then((response) => {
+        setProductsList(response.data);
+      });
+  }, []);
+
   return (
     <div>
-      Related Products Here
+      <h2>Products List</h2>
+      {productsList.map((element) => <Card product={element} />)}
     </div>
   );
 }
