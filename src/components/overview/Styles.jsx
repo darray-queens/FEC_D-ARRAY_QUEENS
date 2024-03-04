97919945e280b381492e6c4b4ce12c990aad18ec
jreagant <jr.tankersley@gmail.com>
2024-03-03 20:05:04 -0500

    stubbed Styles sub-component

---
+++ src/components/overview/Styles.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Styles() {
+  return (
+    <div>
+      <p>Style 1</p>
+      <p>Style 2</p>
+      <p>Style 3</p>
+      <p>Style 4</p>
+    </div>
+  );
+}
+
+export default Styles;
