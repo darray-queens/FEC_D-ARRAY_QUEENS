1ddc3ec2b596eb983fbc14cadc9792aeecf7ce1a
Andrew <andrewpark0408@DESKTOP-H04QUCF.localdomain>
2024-03-04 10:25:26 -0800

    adding functional GetAllQuestions to my fearture

--- src/components/questionsAndAnswers/GetAllQuestions.jsx
+++ src/components/questionsAndAnswers/GetAllQuestions.jsx
@@ -1,29 +1,56 @@
 import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
 import axios from 'axios';
 
-function GetAllQuestions({ productId }) {
+function GetAllQuestions({ currentProduct }) {
   const [questions, setQuestions] = useState([]);
 
   useEffect(() => {
-    const fetchQuestions = async () => {
-      try {
-        const response = await axios.get('https://app-hrsei-api.herokuapp.com/api/fec2/:hr-rfp/', {
-          params: {
-            product_id: productId,
-          }
+    if (currentProduct && currentProduct.id) {
+      const url = '/qa/questions';
+
+      console.log('current Prod:', currentProduct);
+
+      axios.get(url, {
+        params: {
+          product_id: currentProduct.id,
+        },
+        headers: {
+          Authorization: `${process.env.TOKEN}`,
+        },
+      })
+        .then((response) => {
+          setQuestions(response.data.results);
         })
-      } catch (error) {
-        console.error('failed to fetch questions:', error);
-      }
-    };
-    fetchQuestions();
-  }, [productId]);
+        .catch((err) => {
+          console.error('failed to set list: ', err);
+        });
+    }
+  }, [currentProduct]); // Corrected dependency array
 
   return (
     <div>
       <h2>Questions List</h2>
+      <ul>
+        {questions.map((question) => (
+          <li key={question.question_id}>
+            <p>
+              <strong>Question:</strong>
+              {question.question_body}
+            </p>
+            <p>
+              <strong>Asked by:</strong>
+              {question.asker_name}
+            </p>
+            <p>
+              <strong>Helpfulness:</strong>
+              {question.question_helpfulness}
+            </p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
 
-export default GetAllQuestions
+export default GetAllQuestions;
