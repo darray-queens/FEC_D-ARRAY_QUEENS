718300aa6b990af8f45fa522adefd310b345653e
jreagant <jr.tankersley@gmail.com>
2024-03-04 21:52:40 -0500

    added share buttons

--- src/components/overview/Overview.jsx
+++ src/components/overview/Overview.jsx
@@ -4,6 +4,7 @@ import axios from 'axios';
 import Images from './Images';
 import StarRating from './StarRating';
 import ProductInfo from './ProductInfo';
+import Share from './Share';
 import Styles from './Styles';
 import Selection from './Selection';
 import ProductDescription from './ProductDescription';
@@ -41,6 +42,7 @@ function Overview(props) {
         <Col size={1}>
           <StarRating scrollMethod={scrollMethod} />
           <ProductInfo currentProduct={currentProduct} />
+          <Share currentProduct={currentProduct} />
           {isLoading ? (
             <Loading />
           ) : (
---
+++ src/components/overview/Share.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+// window.location.href
+
+/**
+ * <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-show-count="false">
+ * Tweet
+ * </a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+ */
+
+function Share(props) {
+  const { currentProduct } = props;
+  console.log(currentProduct);
+
+  const fbShareLink = `https://www.facebook.com/sharer/sharer.php?u=${window.location.href}`;
+  const twitterShareLink = `http://twitter.com/share?text=check out this ${currentProduct.name}&url=${window.location.href}`.replace(/ /g, '%20');
+  console.log(twitterShareLink);
+
+  return (
+    <div>
+      <a href={fbShareLink} target="blank">
+        <button type="button" name="Facebook">
+          <img alt="Share to Facebook" src="https://cdn0.iconfinder.com/data/icons/social-network-24/512/Facebook-512.png" width="15px" />
+        </button>
+      </a>
+      <a href={twitterShareLink} target="blank">
+        <button type="button" name="Pinterest">
+          <img alt="Share to Pinterest" src="https://cdn1.iconfinder.com/data/icons/free-social-media-4/32/pinterest_social_Media_Logo_share-512.png" width="15px" />
+        </button>
+      </a>
+      <button type="button" name="Twitter">
+        <img alt="Share to Twitter" src="https://cdn4.iconfinder.com/data/icons/viiva-social-media/32/twitter-512.png" width="15px" />
+      </button>
+    </div>
+  );
+}
+
+export default Share;
