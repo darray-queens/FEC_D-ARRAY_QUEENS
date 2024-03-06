494e4f640834ec45dd31db819dbabee10e53a700
jreagant <jr.tankersley@gmail.com>
2024-03-06 09:24:06 -0500

    1 post per item count, conditional styling on buttons

--- src/components/overview/Selection.jsx
+++ src/components/overview/Selection.jsx
@@ -3,25 +3,17 @@ import axios from 'axios';
 
 const { useState } = React;
 
-const buildQuantityList = (qty) => {
-  const list = [];
-  const max = qty > 15 ? 15 : qty;
-  for (let i = 1; i <= max; i += 1) {
-    list.push(<option key={i}>{i}</option>);
-  }
-  return list;
-};
-
 function Selection({ style, sku, changeSku }) {
   const skuTuples = Object.entries(style.skus);
   const [currentQty, setCurrentQty] = useState();
 
   const addToCart = async () => {
-    await axios.post('/cart', { sku_id: sku })
-      .then((response) => console.log(response))
-      .catch((error) => console.error(error));
-
-    changeSku();
+    let count = currentQty;
+    while (count > 0) {
+      axios.post('/cart', { sku_id: sku })
+        .catch((error) => console.error(error));
+      count -= 1;
+    }
   };
 
   const skuSizeOptions = skuTuples.map((skuTuple) => {
@@ -43,9 +35,21 @@ function Selection({ style, sku, changeSku }) {
     </>
   );
 
+  let hideCart = false;
+
   if (skuSizeOptions.length === 0) {
     sizeOptions = <option style={{ display: 'none' }}>OUT OF STOCK</option>;
+    hideCart = true;
   }
+
+  const buildQuantityList = (qty) => {
+    const list = [];
+    const max = qty > 15 ? 15 : qty;
+    for (let i = 1; i <= max; i += 1) {
+      list.push(<option key={i}>{i}</option>);
+    }
+    return list;
+  };
 
   const quantity = sku ? style.skus[sku].quantity : 0;
   const quantityOptions = sku ? buildQuantityList(quantity) : <option>{}</option>;
@@ -58,10 +62,17 @@ function Selection({ style, sku, changeSku }) {
       >
         {sizeOptions}
       </select>
-      <select name="quantity" onChange={(e) => setCurrentQty(e.target.value)}>
+      <select disabled={!sku} name="quantity" onChange={(e) => setCurrentQty(e.target.value)}>
         {quantityOptions}
       </select>
-      <button name="add" type="button" onClick={() => addToCart()}>Add to Bag</button>
+      <button
+        name="add"
+        type="button"
+        style={hideCart ? { display: 'none' } : { display: 'inherit' }}
+        onClick={() => addToCart()}
+      >
+        Add to Bag
+      </button>
     </div>
   );
 }
