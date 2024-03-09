bbd11e2968635180c6a89ed2bad4b2d2e0bf83b5
jreagant <jr.tankersley@gmail.com>
2024-03-09 14:30:29 -0500

    styled product info and sale render

--- src/components/overview/ProductInfo.jsx
+++ src/components/overview/ProductInfo.jsx
@@ -1,23 +1,42 @@
 import React from 'react';
+import styled from 'styled-components';
+
+import { Row } from '../shared/containers';
+
+const Category = styled.h4`
+  margin: 0;
+  padding: 20px 0px 0px 0px;
+  text-transform: uppercase;
+`;
+
+const ProductName = styled.h1`
+  margin: 0;
+  padding: 0;
+`;
+
+const SaleText = styled.h4`
+  color: red;
+  margin-right: 10px;
+`;
 
 function ProductInfo({ product, style }) {
   const { category, name } = product;
 
-  let priceText = <h5>{style.original_price}</h5>;
+  let priceText = <h4>{`$${style.original_price}`}</h4>;
 
   if (style.sale_price) {
     priceText = (
-      <>
-        <h5 style={{ color: 'red' }}>{style.sale_price}</h5>
-        <h5 style={{ textDecoration: 'line-through' }}>{style.original_price}</h5>
-      </>
+      <Row>
+        <SaleText>{`$${style.sale_price}`}</SaleText>
+        <h4 style={{ textDecoration: 'line-through' }}>{`$${style.original_price}`}</h4>
+      </Row>
     );
   }
 
   return (
     <div>
-      <h5>{category}</h5>
-      <h1>{name}</h1>
+      <Category>{category}</Category>
+      <ProductName>{name}</ProductName>
       {priceText}
     </div>
   );
