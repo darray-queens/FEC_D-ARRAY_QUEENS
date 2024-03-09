1c551e8951aa247bcfcaa0aaa5bb755230f49e5b
jreagant <jr.tankersley@gmail.com>
2024-03-09 01:45:42 -0500

    removed unused state

--- src/App.jsx
+++ src/App.jsx
@@ -17,8 +17,6 @@ function App() {
   const [mainImageIndex, setMainImageIndex] = useState(0);
   const [minThumbIndex, setMinThumbIndex] = useState(0);
   const [maxThumbIndex, setMaxThumbIndex] = useState();
-  const [imageShiftX, setImageShiftX] = useState();
-  const [imageShiftY, setImageShiftY] = useState();
   const [galleryModal, setGalleryModal] = useState(false);
 
   useEffect(() => {
