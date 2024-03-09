ee1dd050ae0aa6e6719b34191d5e9fbfc831f4ca
jreagant <jr.tankersley@gmail.com>
2024-03-09 15:50:46 -0500

    styled social buttons

--- src/components/overview/Share.jsx
+++ src/components/overview/Share.jsx
@@ -1,38 +1,55 @@
 import React from 'react';
+import styled from 'styled-components';
+import { FaFacebook, FaTwitter, FaPinterest } from 'react-icons/fa';
 
-// window.location.href
+import { Row } from '../shared/containers';
 
-/**
- * <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-show-count="false">
- * Tweet
- * </a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
- */
+const ShareRow = styled(Row)`
+  max-width: 100%;
+`;
+
+const ShareLink = styled.a`
+  color: black;
+  margin-right: 8px;
+`;
+
+const FbShareLink = styled(ShareLink)`
+  &:hover {
+    color: #316FF6;
+  }
+`;
+
+const TwShareLink = styled(ShareLink)`
+  &:hover {
+    color: #1DA1F2;
+  }
+`;
+
+const PinShareLink = styled(ShareLink)`
+  &:hover {
+    color: #E60023;
+  }
+`;
 
 function Share(props) {
   const { currentProduct, styleImage } = props;
 
-  const fbShareLink = `https://www.facebook.com/sharer/sharer.php?u=${window.location.href}`;
-  const twitterShareLink = `http://twitter.com/share?text=check out this ${currentProduct.name}&url=${window.location.href}`.replace(/ /g, '%20');
-  const pinterestShareLink = `https://www.pinterest.com/pin/create/button/?url=${window.location.href}&media=${styleImage}`;
+  const fbUrl = `https://www.facebook.com/sharer/sharer.php?u=${window.location.href}`;
+  const twitterUrl = `http://twitter.com/share?text=check out this ${currentProduct.name}&url=${window.location.href}`.replace(/ /g, '%20');
+  const pinterestUrl = `https://www.pinterest.com/pin/create/button/?url=${window.location.href}&media=${styleImage}`;
 
   return (
-    <div>
-      <a href={fbShareLink} target="blank">
-        <button type="button" name="Facebook">
-          <img alt="Share to Facebook" src="https://cdn0.iconfinder.com/data/icons/social-network-24/512/Facebook-512.png" width="15px" />
-        </button>
-      </a>
-      <a href={twitterShareLink} target="blank">
-        <button type="button" name="Pinterest">
-          <img alt="Share to Pinterest" src="https://cdn1.iconfinder.com/data/icons/free-social-media-4/32/pinterest_social_Media_Logo_share-512.png" width="15px" />
-        </button>
-      </a>
-      <a href={pinterestShareLink} target="blank">
-        <button type="button" name="Twitter">
-          <img alt="Share to Twitter" src="https://cdn4.iconfinder.com/data/icons/viiva-social-media/32/twitter-512.png" width="15px" />
-        </button>
-      </a>
-    </div>
+    <ShareRow>
+      <FbShareLink href={fbUrl} target="blank">
+        <FaFacebook />
+      </FbShareLink>
+      <TwShareLink href={twitterUrl} target="blank">
+        <FaTwitter />
+      </TwShareLink>
+      <PinShareLink href={pinterestUrl} target="blank">
+        <FaPinterest />
+      </PinShareLink>
+    </ShareRow>
   );
 }
 
