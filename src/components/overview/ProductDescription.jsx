6fa1ef9524817e9a9f07991ad28caa7489415acf
jreagant <jr.tankersley@gmail.com>
2024-03-09 15:02:40 -0500

    refactored, styled product description and features

--- src/components/overview/ProductDescription.jsx
+++ src/components/overview/ProductDescription.jsx
@@ -1,28 +1,54 @@
 import React from 'react';
+import styled from 'styled-components';
 
-function ProductDescription(props) {
-  const { description, features } = props;
+import { Row, Col } from '../shared/containers';
 
-  let highlights = [];
-  if (features) { highlights = features; }
+const DescriptionRow = styled(Row)`
+  align-items: center;
+  padding-top: 20px;
+  max-width: 1080px;
+  `;
 
+const DescriptionCol = styled(Col)`
+  min-width: 480px;
+  margin-right: 20px;
+`;
+
+const FeatureCol = styled(Col)`
+  align-self: flex-start;
+  font-size: 14px;
+  font-weight: bold;
+`;
+
+const LineCol = styled(Col)`
+  background: #d3d3d3;
+  height: 80%;
+  width: 2px;
+`;
+
+function ProductDescription({ description, features }) {
   let featureKey = 0;
 
   return (
-    <div>
-      <p>{description}</p>
-      <ul>
-        {highlights.map((details) => {
-          featureKey += 1;
-          const text = `${details.feature} - ${details.value}`;
-          return (
-            <li key={featureKey}>
-              {text}
-            </li>
-          );
-        })}
-      </ul>
-    </div>
+    <DescriptionRow>
+      <DescriptionCol size={3}>
+        <p>{description}</p>
+      </DescriptionCol>
+      <LineCol />
+      <FeatureCol size={1}>
+        <ul>
+          {features && features.map((details) => {
+            featureKey += 1;
+            const text = `${details.feature} - ${details.value}`;
+            return (
+              <li key={`feature #${featureKey}`}>
+                {text}
+              </li>
+            );
+          })}
+        </ul>
+      </FeatureCol>
+    </DescriptionRow>
   );
 }
 
