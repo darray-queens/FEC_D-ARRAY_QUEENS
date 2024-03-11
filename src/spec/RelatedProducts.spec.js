dedd56882237cc283f4f28dd791e43a8361c4f60
Amarin Sam <amarinsam@gmail.com>
2024-03-10 20:40:51 -0700

    Updated test suite

--- src/spec/RelatedProducts.spec.js
+++ src/spec/RelatedProducts.spec.js
@@ -10,26 +10,26 @@ import productSample from './sampleData';
 import sampleProductData from './sampleProductData';
 import sampleStyleData from './sampleStyleData';
 import sampleReviewData from './sampleReviewData';
+import sampleComparedItems from './sampleComparedItems';
+import {product1Features, product2Features} from './sampleProductFeaturesData';
 
 import RelatedProductsList from '../components/relatedProductsModule/RelatedProducts/RelatedProductsList';
+import ComparisonModule from '../components/relatedProductsModule/RelatedProducts/ComparisonModule';
 
 jest.mock('axios');
-
-beforeEach(() => {
-  axios.get.mockResolvedValueOnce({data: sampleProductData})
-  axios.get.mockResolvedValueOnce({data: sampleStyleData})
-  axios.get.mockResolvedValueOnce({data: sampleReviewData})
-  axios.get.mockResolvedValueOnce({data: sampleStyleData})
-})
 
 afterEach(() => {
   jest.clearAllMocks();
 });
 
 describe('RelatedProductsList', () => {
+  axios.get.mockResolvedValueOnce({data: sampleProductData})
+  axios.get.mockResolvedValueOnce({data: sampleStyleData})
+  axios.get.mockResolvedValueOnce({data: sampleReviewData})
+  axios.get.mockResolvedValueOnce({data: sampleStyleData})
+
   it('renders RelatedProductsList component', async () => {
     render(<RelatedProductsList currentProduct={productSample}/>);
-
 
       expect(screen.getByText('Loading...')).toBeTruthy();
 
@@ -41,5 +41,34 @@ describe('RelatedProductsList', () => {
 
     })
 
+    axios.get.mockResolvedValueOnce({data: product1Features});
+    axios.get.mockResolvedValueOnce({data: product2Features});
+
+
+    it('opens the Comparison Module when two stars are clicked', async () => {
+      render(<ComparisonModule comparedItems={sampleComparedItems} />)
+
+      expect(screen.getByText('Comparing')).toBeTruthy();
+
+      await waitFor(() => {
+        expect(screen.getByText('Carter Jacket')).toBeTruthy();
+      })
+
+      await waitFor(() => {
+        expect(screen.getByText('Zaria Jacket')).toBeTruthy();
+      })
+
+    })
+
+    it('closes the Comparison Module when clicked away', async () => {
+
+    })
+
 });
 
+// ComparisonModule.jsx
+// RelatedProductsList.jsx
+// ProductCard.jsx
+// fetchSalePrice.jsx
+// scrollButtonClick.js
+//
