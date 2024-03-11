aa0e224c70f97fda3ec53da9d28e8b2919628116
jreagant <jr.tankersley@gmail.com>
2024-03-11 10:51:54 -0400

    styled gallery thumbnail buttons

--- src/components/overview/ThumbnailMenu.jsx
+++ src/components/overview/ThumbnailMenu.jsx
@@ -1,6 +1,23 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import { Grid, Row } from '../shared/containers';
+
+const ThumbButton = styled.button`
+background: none;
+background-color: none;
+margin-bottom: 6px;
+margin-left: 12px;
+margin-right: 6px;
+padding: none;
+border: none;
+height: 50px;
+width: 50px;
+background-size: cover;
+&:hover {
+  cursor: pointer;
+}
+`;
 
 function ThumbnailMenu({
   styleImages,
@@ -12,22 +29,24 @@ function ThumbnailMenu({
     <Grid>
       {styleImages.map((image, index) => (
         <Row key={`Style #${index}`}>
-          <button
+          <ThumbButton
             name="galleryThumbnail"
             type="button"
+            data-index={index}
             style={{
               display: index >= minThumbIndex && index <= maxThumbIndex ? 'inherit' : 'none',
+              backgroundImage: `url(${image.thumbnail_url})`,
             }}
             onClick={(e) => changeMainImageIndex(Number(e.target.dataset.index))}
           >
-            <img
+            {/* <img
               data-index={index}
               alt="The next addition to your wardrobe"
               src={image.thumbnail_url}
               height="50px"
               width="50px"
-            />
-          </button>
+            /> */}
+          </ThumbButton>
         </Row>
       ))}
     </Grid>
