b11204a1897a85b2f4473052db7482878f70c6cd
jreagant <jr.tankersley@gmail.com>
2024-03-10 20:00:28 -0400

    finished styling size, quantity selects

--- src/components/overview/Selection.jsx
+++ src/components/overview/Selection.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import axios from 'axios';
 import styled from 'styled-components';
+import { Row } from '../shared/containers';
 
 const { useState } = React;
 
 const StyleSelect = styled.select`
-  padding-left: 12px;
+  appearance: none;
+  display: block;
+  padding-left: 20px;
   padding-right: 10px;
   margin-right: 20px;
   font-family: inherit;
@@ -17,14 +20,23 @@ const StyleSelect = styled.select`
   width: 150px;
   &:hover {
     background-color: rgb(220,220,220);
+    cursor: pointer;
   }
   &:focus {
     border-radius: 0%;
     border-width: 2px;
+    outline: 0;
   }
 `;
 
-const StyleOption = styled.option`
+const QuantSelect = styled(StyleSelect)`
+  padding-left: 20px;
+  margin-right: 20px;
+  width: 55px;
+`;
+
+const SelectOption = styled.option`
+  text-align: left;
   padding: 10px;
   margin-top: 60px;
   margin-bottom: 20px;
@@ -63,7 +75,7 @@ function Selection({ style, sku, changeSku }) {
     const { size, quantity } = skuTuple[1];
     if (quantity > 0) {
       return (
-        <StyleOption key={code} data-sku={code} id={size}>{size}</StyleOption>
+        <SelectOption key={code} data-sku={code} id={size}>{size}</SelectOption>
       );
     }
 
@@ -72,7 +84,7 @@ function Selection({ style, sku, changeSku }) {
 
   let sizeOptions = (
     <>
-      <StyleOption style={{ display: 'none' }}>SELECT SIZE</StyleOption>
+      <SelectOption style={{ display: 'none' }}>SELECT SIZE &#129139;</SelectOption>
       {skuSizeOptions}
     </>
   );
@@ -80,7 +92,7 @@ function Selection({ style, sku, changeSku }) {
   let hideCart = false;
 
   if (skuSizeOptions.length === 0) {
-    sizeOptions = <StyleOption style={{ display: 'none' }}>OUT OF STOCK</StyleOption>;
+    sizeOptions = <SelectOption style={{ display: 'none' }}>OUT OF STOCK</SelectOption>;
     hideCart = true;
   }
 
@@ -88,39 +100,41 @@ function Selection({ style, sku, changeSku }) {
     const list = [];
     const max = qty > 15 ? 15 : qty;
     for (let i = 1; i <= max; i += 1) {
-      list.push(<option key={i}>{i}</option>);
+      list.push(<SelectOption key={i}>{i}</SelectOption>);
     }
     return list;
   };
 
   const quantity = sku ? style.skus[sku].quantity : 0;
-  const quantityOptions = sku ? buildQuantityList(quantity) : <option>{}</option>;
+  const quantityOptions = sku ? buildQuantityList(quantity) : <SelectOption>{}</SelectOption>;
 
   return (
     <div>
       <h5 style={hideLabel ? { display: 'none' } : { display: 'inherit' }}>Please select a size</h5>
-      <StyleSelect
-        name="size"
-        id="size-select"
-        onChange={(e) => {
-          changeSku(document.getElementById(e.target.value).dataset.sku);
-          setHideLabel(true);
-        }}
-      >
-        {sizeOptions}
-      </StyleSelect>
-      <select disabled={!sku} name="quantity" onChange={(e) => setCurrentQty(e.target.value)}>
-        <option style={{ display: 'none' }}>{}</option>
-        {quantityOptions}
-      </select>
-      <button
-        name="add"
-        type="button"
-        style={hideCart ? { display: 'none' } : { display: 'inherit' }}
-        onClick={() => addToCartHandler()}
-      >
-        Add to Bag
-      </button>
+      <Row>
+        <StyleSelect
+          name="size"
+          id="size-select"
+          onChange={(e) => {
+            changeSku(document.getElementById(e.target.value).dataset.sku);
+            setHideLabel(true);
+          }}
+        >
+          {sizeOptions}
+        </StyleSelect>
+        <QuantSelect disabled={!sku} name="quantity" onChange={(e) => setCurrentQty(e.target.value)}>
+          <SelectOption style={{ display: 'none' }}>&#129139;</SelectOption>
+          {quantityOptions}
+        </QuantSelect>
+        <button
+          name="add"
+          type="button"
+          style={hideCart ? { display: 'none' } : { display: 'inherit' }}
+          onClick={() => addToCartHandler()}
+        >
+          Add to Bag
+        </button>
+      </Row>
     </div>
   );
 }
