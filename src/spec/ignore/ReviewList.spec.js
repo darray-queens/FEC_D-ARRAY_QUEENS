ef70ccffd9a2bad9e75750caf4bfee2cdec1eb0d
Malcolm Kam <malcolm.h.kam@gmail.com>
2024-03-12 08:27:46 -1000

    70% coverage for review component

--- src/spec/ignore/ReviewList.spec.js
+++ src/spec/ignore/ReviewList.spec.js
@@ -4,14 +4,20 @@
 
 import * as React from 'react';
 import { render, screen } from '@testing-library/react';
-
 import productSample from './sampleData';
-
 import ReviewList from '../components/review/ReviewList';
+import axios from 'axios'
+import sampleReviews from './sampleReviews';
+
+jest.mock('axios')
+
+const { useState } = React;
 
 describe('ReviewList', () => {
+  const [reviews, setReviews] = useState([]);
+  axios.get.mockResolvedValueOnce({data: sampleReviews});
   it('renders ReviewList component', () => {
-    render(<ReviewList currentProduct={productSample}/>);
-    screen.debug();
+    render(<ReviewList currentProduct={productSample} reviews={[]} updateReviews={(newReviews) => setReviews(newReviews)}/>);
+    expect(screen.getByText('No reviews loaded')).toBeTruthy();
   });
 });
